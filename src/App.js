10a3a779a8dd5c400f1a0838c6904c855c6c1584
nj2397 <nibjyotin.m@yahoo.com>
2024-07-01 23:30:35 +0530

    feat(digital clock development): implementation of digital clock development

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import DClock from "./screen/DClock"
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -12,12 +13,16 @@ function App() {
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
+          target="_blank"s
           rel="noopener noreferrer"
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <DClock />
+
+
     </div>
   );
 }
